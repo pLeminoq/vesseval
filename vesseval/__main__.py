1f533b3a7fdf6e52848bd52fb18ac9eae2201c97
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2024-07-29 14:12:31 +0200

    introduce rectangle mode to main window

--- vesseval/__main__.py
+++ vesseval/__main__.py
@@ -7,30 +7,17 @@ import numpy as np
 from .state import PointState
 from .state.app import app_state
 from .widgets.canvas.image import Image, ImageState
-from .widgets.canvas.rectangle import Rectangle, RectangleState
-from .widgets.canvas.line import Line, LineState
 from .widgets.canvas.contour import Contour, ContourState
 from .widgets.menu import MenuBar
 from .widgets.toolbar import Toolbar
-
-# class App(tk.Tk):
-
-# def __init__(self):
-
-# c = tk.Canvas(
-# self,
-# width=img.shape[1],
-# height=img.shape[0],
-# )
-# self.image = Image(self.canvas, ImageState(img))
-
-# self.canvas.grid(column=0, row=0)
+from .test import ThresholdView
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", type=str, default="")
 args = parser.parse_args()
 
 app_state.filename_state.set(args.image)
+
 
 class App(tk.Tk):
 
@@ -54,12 +41,20 @@ class App(tk.Tk):
         self.contour = None
 
         self.bind("<Key-q>", lambda event: exit(0))
+        self.bind("<Return>", self.on_return)
 
     def on_bb_mode(self, state):
         if state.value:
-            self.bb_mode_bindings["<Button-1>"] = self.canvas.bind("<Button-1>", self.bb_mode_new_contour)
-            self.bb_mode_bindings["<B1-Motion>"] = self.canvas.bind("<B1-Motion>", self.bb_mode_init_contour)
-            self.bb_mode_bindings["<B1-ButtonRelease>"] = self.canvas.bind("<B1-ButtonRelease>", lambda event: self.toolbar.state.mouse_mode.set(True))
+            self.bb_mode_bindings["<Button-1>"] = self.canvas.bind(
+                "<Button-1>", self.bb_mode_new_contour
+            )
+            self.bb_mode_bindings["<B1-Motion>"] = self.canvas.bind(
+                "<B1-Motion>", self.bb_mode_init_contour
+            )
+            self.bb_mode_bindings["<B1-ButtonRelease>"] = self.canvas.bind(
+                "<B1-ButtonRelease>",
+                lambda event: self.toolbar.state.mouse_mode.set(True),
+            )
         else:
             for binding, _id in self.bb_mode_bindings.items():
                 self.canvas.unbind(binding, _id)
@@ -67,10 +62,17 @@ class App(tk.Tk):
 
     def bb_mode_new_contour(self, event):
         if self.contour is not None:
-            self.contour.clear() 
+            self.contour.clear()
 
         x, y = event.x, event.y
-        self.contour_state = ContourState([PointState(x, y), PointState(x + 1, y), PointState(x + 1, y + 1), PointState(x, y + 1)])
+        self.contour_state = ContourState(
+            [
+                PointState(x, y),
+                PointState(x + 1, y),
+                PointState(x + 1, y + 1),
+                PointState(x, y + 1),
+            ]
+        )
         self.contour = Contour(self.canvas, self.contour_state)
 
     def bb_mode_init_contour(self, event):
@@ -82,6 +84,20 @@ class App(tk.Tk):
         self.contour_state[2].y.set(y)
 
         self.contour_state[3].y.set(y)
+
+    def on_return(self, *args):
+        img = app_state.image_state.value
+        cnt = self.contour_state.to_numpy()
+
+        mask = np.zeros(img.shape[:2], np.uint8)
+        mask = cv.drawContours(mask, [cnt], 0, 255, -1)
+
+        l, t, w, h = cv.boundingRect(cnt)
+        _img = cv.bitwise_and(img, img, mask=mask)
+        _img = _img[t : t + h, l : l + w]
+
+        ThresholdView(_img)
+
 
 app = App()
 app.mainloop()
