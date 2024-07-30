de0202e0786025d23330ff83bbfd8e87e30d4204
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2024-07-30 15:18:54 +0200

    fix super __init__ call in label

--- vesseval/widgets/label.py
+++ vesseval/widgets/label.py
@@ -6,7 +6,7 @@ from ..state import StringState
 class Label(tk.Label):
 
     def __init__(self, parent: tk.Widget, state: StringState, **kwargs):
-        super().__init__(parent, text=state.value, *kwargs)
+        super().__init__(parent, text=state.value, **kwargs)
 
         self.state = state
         self.state.on_change(lambda state: self.config(text=state.value))
