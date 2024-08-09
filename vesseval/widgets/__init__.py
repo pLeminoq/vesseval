8f990e9434e46f2289dc200e093f07177c0835e4
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2024-08-09 14:41:16 +0200

    add __init__ script for widgets packages

---
+++ vesseval/widgets/__init__.py
@@ -0,0 +1,18 @@
+from .chechbox import Checkbox, CheckboxState
+from .label import Label
+from .scale import Scale, ScaleState
+from .table import Table, RowState, TableState
+from .textfield import TextField, IntTextField
+
+__all__ = [
+    "Checkbox",
+    "CheckboxState",
+    "Label",
+    "Scale",
+    "ScaleState",
+    "Table",
+    "RowState",
+    "TableState",
+    "TextField",
+    "IntTextField",
+]
---
+++ vesseval/widgets/canvas/__init__.py
@@ -0,0 +1,14 @@
+from .contour import Contour, DisplayContourState
+from .image import Image
+from .line import Line, LineState
+from .rectangle import Rectangle, RectangleState
+
+__all__ = [
+    "Contour",
+    "DisplayContourState",
+    "Image",
+    "Line",
+    "LineState",
+    "Rectangle",
+    "RectangleState",
+]
