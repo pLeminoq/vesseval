d5414fae9b16801843da39dfa5bb706dca334158
Tamino Huxohl <thuxohl@techfak.uni-bielefeld.de>
2024-12-03 11:57:24 +0100

    add default export of bounding box state

--- vesseval/state/__init__.py
+++ vesseval/state/__init__.py
@@ -1,11 +1,14 @@
 """
 States enable a reactive implementation of widgets.
 """
+
+from .bounding_box import BoundingBoxState
 from .contour import ContourState
 from .image import DisplayImageState, ImageState, ResolutionState, ImageConfigState
 from .point import PointState
 
 __all__ = [
+    "BoundingBoxState",
     "ContourState",
     "DisplayImageState",
     "ImageState",
